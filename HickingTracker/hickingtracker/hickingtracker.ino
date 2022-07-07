1f7141ecc41cb977a4790d4c4ea9f50cc8b52dbb
guilleber <guillaume.le_berre@depinfonancy.net>
2022-07-06 22:12:37 -0400

    basic interupt test

--- HickingTracker/hickingtracker/hickingtracker.ino
+++ HickingTracker/hickingtracker/hickingtracker.ino
@@ -1,23 +1,25 @@
 #include "Sensors.h"
 
+#define INT_PIN 4
+
 
 Sensors sensors;
+
+volatile int state = 0;
+
+void plop() {
+  state = (state+1)%5;
+}
 
 void setup() {
   Serial.begin(9600);
   sensors = Sensors();
+  pinMode(INT_PIN, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), plop, FALLING);
 }
 
 void loop() {
-  float TX = sensors.getTemperature();
-
-  Serial.print("Temperature:");
-  Serial.print("\t");
-  Serial.print(TX);
-  Serial.print("C\t\t");
-  Serial.print(TX + 273.15);        //Conversion to Kelvin
-  Serial.print("K\t\t");
-  Serial.print((TX * 1.8) + 32);    //Conversion to Fahrenheit
-  Serial.println("F");
   delay(1000);
+  Serial.print(state);
+  Serial.print("\n");
 }
